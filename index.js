be21b55122cf39720d96893977d45d900695359a
Xatpy <jaime.chapinal@gmail.com>
2016-02-24 23:24:14 +0100

    Adding sound management from server.

--- index.js
+++ index.js
@@ -7,21 +7,41 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  	res.sendfile('index.html');
+  	console.log("ja");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+// Sounds by default
+var obj = {};
+var id1 = {"id":1, "name":"hola", "sound": "media/glass.mp3", "img": "media/vaso.jpg"};
+var id2 = {"id":2, "name":"adios", "sound": "media/laugh.mp3", "img": "media/risa.jpg"};
+obj.listSounds = [];
+obj.listSounds.push(id1);
+obj.listSounds.push(id2);
 
-  socket.on('registerSound', function(msg){
-    //io.emit('chat message', msg);
-    console.log(msg);
-    io.emit('newSound', msg)
-  });
+io.on('connection', function(socket){
+    console.log('New user connected');
+
+    io.emit('newConnection', obj);
+
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+
+	socket.on('registerSound', function(msg){
+		debugger
+		console.log("New sound registered: " + msg);
+		var newSoundObj = { 
+			"id":obj.listSounds.length + 1, 
+			"name": msg, 
+			"sound": msg, 
+			"img": "img"
+		};
+		obj.listSounds.push(newSoundObj);
+		io.emit('newSound', newSoundObj);
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+ 	console.log('listening on *:3000');
 });
