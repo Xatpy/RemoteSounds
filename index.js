eb614c507e2c806cda089d76674c7d4d3db88daa
Xatpy <jaime.chapinal@gmail.com>
2016-03-14 21:23:59 +0100

    Adding sounds and refactoring

--- index.js
+++ index.js
@@ -15,17 +15,23 @@ app.get('/', function(req, res){
 
 // Sounds by default
 var obj = {};
-var id1 = {"id":0, "name":"vaso", "sound": "media/glass.mp3", "img": "media/vaso.jpg"};
-var id2 = {"id":1, "name":"risa", "sound": "media/laugh.mp3", "img": "media/risa.jpg" };
+var id0 = {"id":0, "name":"Crystal crash", "sound": "media/glass.mp3", "img": "media/vaso.jpg"};
+var id1 = {"id":1, "name":"LOL", "sound": "media/laugh.mp3", "img": "media/risa.jpg" };
+var id2 = {"id":2, "name":"Applause", "sound": "media/applause.mp3", "img": "media/applause.png" };
+var id3 = {"id":3, "name":"Gum Bubble", "sound": "media/gumBubble.mp3", "img": "media/boomer.jpg" };
+
+
 obj.listSounds = [];
+obj.listSounds.push(id0);
 obj.listSounds.push(id1);
 obj.listSounds.push(id2);
+obj.listSounds.push(id3);
 
 var connectedUsers = 0;
 
 
 io.on('connection', function(socket){
-    console.log('New user connected');
+    console.log(getDateTime() + ' - New user connected');
     connectedUsers++;
     io.emit('newConnection', obj, connectedUsers);
 
@@ -34,9 +40,8 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('registerSound', function(soundURL, imageURL, name){
-		debugger
-		console.log("New sound registered: " + soundURL);
-		console.log("New image registered: " + imageURL);
+		console.log(getDateTime() + " - New sound registered: " + soundURL);
+		console.log(getDateTime() + " - New image registered: " + imageURL);
 		var newSoundObj = { 
 			"id":obj.listSounds.length + 1, 
 			"name": name, 
@@ -48,12 +53,36 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('disconnect', function(numUsers) { 
-		console.log('User disconnected');
+		console.log(getDateTime() + ' - User disconnected');
 		connectedUsers--;
 		io.emit('disconnect', connectedUsers);
 	});
 });
 
 http.listen(port, function(){
- 	console.log('listening on *:3000');
+ 	console.log('listening on *:' + port);
 });
+
+
+function getDateTime() {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+}
